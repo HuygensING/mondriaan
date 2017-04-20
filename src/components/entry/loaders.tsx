46ef103c2ddef3619f6265864177163f1a076c8c
Gijsjan <agijsbro@gmail.com>
2017-04-20 10:04:10 +0200

    Bump mondrian-entries

--- src/components/entry/loaders.tsx
+++ src/components/entry/loaders.tsx
@@ -1,9 +1,11 @@
+const loadSchetsboek1 = require('bundle-loader?lazy!entries/Schetsboek-1');
 const loadSchetsboek2 = require('bundle-loader?lazy!entries/Schetsboek-2');
 const loadNbis = require('bundle-loader?lazy!entries/1917_NieuweBeeldingInSchilderkunst_STIJL');
 const loadBeo = require('bundle-loader?lazy!entries/1918_BepaaldeEnOnbepaalde_STIJL');
 
 export default {
+	'schetsboek-1': loadSchetsboek1,
 	'schetsboek-2': loadSchetsboek2,
-	'1917-nieuwebeeldinginschilderkunst-stijl': loadNbis,
-	'1918-bepaaldeenonbepaalde-stijl': loadBeo,
+	'nieuwe-beelding-in-schilderkunst': loadNbis,
+	'bepaalde-en-onbepaalde-stijl': loadBeo,
 };
